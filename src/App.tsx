f3a3e6b01377e586cb844e462950d8de404a28b9
nogueira <marcosrafael.nm62@gmail.com>
2025-05-06 21:18:06 -0300

    feat: added search bar container

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,34 @@ import { Homepage } from "./pages/home/homepage";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+    const [greetMsg, setGreetMsg] = useState("");
+    const [name, setName] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+    async function greet() {
+        // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+        setGreetMsg(await invoke("greet", { name }));
+    }
 
-  async function hello_fellas(){
-    await invoke("hello_fellas");
-  }
+    async function hello_fellas() {
+        await invoke("hello_fellas");
+    }
 
-  return (
-    <main className="main-container">
-      <div className="navigator">
-        <a> Inicio </a>
-        <a> Configurações </a>
-        <a> Dashboard </a>
-      </div>
-      <Homepage />
-    </main>
-  );
+    return (
+        <div className="main-container">
+            <div className="navigator">
+                <h1>EPIAI</h1>
+                <a> Inicio </a>
+                <a> Configurações </a>
+                <a> Dashboard </a>
+            </div>
+            <div className="app-container">
+                <div className="search-container">
+                    <input type="text" name="" id="" placeholder="pesquisar" />
+                </div>
+                <Homepage />
+            </div>
+        </div>
+    );
 }
 
 export default App;
